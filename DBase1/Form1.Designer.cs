d8df2a3adf7b6d05a4047ba8126344b6771023f4
erolcalisgan <erolcalisgan@hotmail.com>
2018-01-03 16:56:50 +0300

    Tutorials and  Examples

--- DBase1/Form1.Designer.cs
+++ DBase1/Form1.Designer.cs
@@ -31,40 +31,53 @@
             this.btnwrite = new System.Windows.Forms.Button();
             this.btnread = new System.Windows.Forms.Button();
             this.lbl1 = new System.Windows.Forms.Label();
+            this.lstbx = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnwrite
             // 
-            this.btnwrite.Location = new System.Drawing.Point(77, 256);
+            this.btnwrite.Location = new System.Drawing.Point(252, 587);
             this.btnwrite.Name = "btnwrite";
             this.btnwrite.Size = new System.Drawing.Size(121, 33);
             this.btnwrite.TabIndex = 0;
             this.btnwrite.Text = "Yaz";
             this.btnwrite.UseVisualStyleBackColor = true;
+            this.btnwrite.Click += new System.EventHandler(this.btnwrite_Click);
             // 
             // btnread
             // 
-            this.btnread.Location = new System.Drawing.Point(345, 256);
+            this.btnread.Location = new System.Drawing.Point(460, 587);
             this.btnread.Name = "btnread";
             this.btnread.Size = new System.Drawing.Size(121, 33);
             this.btnread.TabIndex = 1;
             this.btnread.Text = "Oku";
             this.btnread.UseVisualStyleBackColor = true;
+            this.btnread.Click += new System.EventHandler(this.btnread_Click);
             // 
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(74, 71);
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(36, 42);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.Size = new System.Drawing.Size(41, 15);
             this.lbl1.TabIndex = 2;
             this.lbl1.Text = "label1";
+            // 
+            // lstbx
+            // 
+            this.lstbx.FormattingEnabled = true;
+            this.lstbx.Location = new System.Drawing.Point(109, 42);
+            this.lstbx.Name = "lstbx";
+            this.lstbx.Size = new System.Drawing.Size(643, 524);
+            this.lstbx.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 395);
+            this.ClientSize = new System.Drawing.Size(781, 664);
+            this.Controls.Add(this.lstbx);
             this.Controls.Add(this.lbl1);
             this.Controls.Add(this.btnread);
             this.Controls.Add(this.btnwrite);
@@ -75,11 +88,17 @@
 
         }
 
+        private void Btnread_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
         #endregion
 
         private System.Windows.Forms.Button btnwrite;
         private System.Windows.Forms.Button btnread;
         private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.ListBox lstbx;
     }
 }
 
--- DBase1/Form1.cs
+++ DBase1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,71 @@ namespace DBase1
         {
             InitializeComponent();
         }
+
+        private void btnwrite_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connect = new SqlConnection())
+            {
+                connect.ConnectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=kutuphane;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                connect.Open();
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO kullanicilar(KULLANICIADI,AD,SOYADI,FAKULTE,BOLUM,NUMARA,SINIF,YETKI) VALUES('osman1970','OSMAN','MERT','MUHENDISLIK FAKULTESI','MAKINE MUHENDISLIGI','B1212.10045','1','0')", connect))
+                {
+                    command.ExecuteNonQuery();
+
+                }//SqlCommand using sonu
+                connect.Close();
+            }//SqlConnection using sonu
+
+        }
+
+
+
+
+
+
+
+        private void btnread_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connect = new SqlConnection())
+            {
+                connect.ConnectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=kutuphane;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                connect.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM kullanicilar", connect))
+                {
+                    SqlDataReader reader=   command.ExecuteReader();
+                    //reader.Read(); //satır1
+                    //reader.Read(); //satır2
+                    lbl1.Text = "";
+                    while (reader.Read())
+                    {
+                        string veri = "\n " + reader.GetString(0);
+                        veri += "\n " + reader.GetString(1);
+                        veri += "\n " + reader.GetString(2);
+                        veri += "\n " + reader.GetString(3);
+                        veri += "\n " + reader.GetString(4);
+                        veri += "\n " + reader.GetString(5);
+
+                        veri += "\n " + reader.GetInt32(6);
+                        veri += "\n " + reader.GetInt32(7);
+                        //lbl1.Text += veri;
+                        lstbx.Items.Add(veri);
+
+
+                    }// while sonu
+
+                   // string veri = reader.GetString(0);
+                 
+                  //  lbl1.Text = veri;
+
+                }//SqlCommand using sonu
+                connect.Close();
+            }//SqlConnection using sonu
+
+        }
+
+        
     }
 }
